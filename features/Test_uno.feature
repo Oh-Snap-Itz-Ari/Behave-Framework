1bf5325975e9bde7275fce6b2f816d443e362977
Alex Fabian Melo <alexfabianmelo12@hotmail.com>
2023-05-27 20:07:28 -0500

    Commit 4

--- features/Test_uno.feature
+++ features/Test_uno.feature
@@ -1,7 +1,16 @@
 Feature: Nuestro primer Demo
 
-  Scenario: Corriendo nuestro primer test
-     Given Abriendo el navegador
-      When Cargando el nombre del usuario
-      Then Cargando su email
-      And Cargando su dirección
+  Background: # Esta función permite que no sea necesario repetir el codigo en todos los Scenarios
+      Given Abriendo el navegador
+
+  Scenario Outline: Corriendo nuestro primer test
+      When Cargando el nombre "<userName>"
+      Then Cargando su "<userEmail>"
+      And Cargando su dirección "<currentAddress>"
+      And Cargando su dirección2 <permanentAddress>
+      And Se da clic al botón de Submit
+
+      Examples:
+      | userName | userEmail | currentAddress | permanentAddress |
+      | MrBlueSky123 | alexfabianmelo12@hotmail.com | Calle 28 # 01 - 26 San Fernando | Calle 28 # 01 - 26 San Fernando |
+      | Insheloots | divher566@gmail.com | Calle 12 # 15 - 12 Centro | Calle 12 # 15 - 12 Centro |
--- features/steps/pasos_test_uno.py
+++ features/steps/pasos_test_uno.py
@@ -1,20 +1,54 @@
 from behave import given, when, then
 
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from Funciones import Funciones_Globales
+
+t = 1
+
 @given(u'Abriendo el navegador')
 def step_impl(context):
-    print(u'STEP: Given Abriendo el navegador')
+    ser = Service("C:\Drivers\chromedriver.exe")
+    op = webdriver.ChromeOptions()
+    global driver, f
+    context.driver = webdriver.Chrome(service=ser, options=op) # 1. Es importante añadir el context antes del driver
+    f = Funciones_Globales(context.driver)
+    f.Navegar("https://demoqa.com/text-box",t)
+
+@when(u'Cargando el nombre "{userName}"')
+def step_impl(context, userName):
+    print(u'STEP: When Cargando el nombre usuario')
+    f.TextoMixto("ID", "userName", userName, t) # 2. Una buena practica es siempre priorizar el ID ante el XPATH o CSS
 
 
-@when(u'Cargando el nombre del usuario')
-def step_impl(context):
-    print(u'STEP: When Cargando el nombre del usuario')
-
-
-@then(u'Cargando su email')
-def step_impl(context):
+@then(u'Cargando su "{userEmail}"')
+def step_impl(context, userEmail):
     print(u'STEP: Then Cargando su email')
+    f.TextoMixto("ID", "userEmail", userEmail, t)
 
 
-@then(u'Cargando su dirección')
+@then(u'Cargando su dirección "{currentAddress}"')
+def step_impl(context, currentAddress):
+    print(u'STEP: Then Cargando su dirección nombre')
+    f.TextoMixto("ID", "currentAddress", currentAddress, t)
+
+@then(u'Cargando su dirección2 {permanentAddress}')
+def step_impl(context, permanentAddress):
+    print(u'STEP: Then Cargando su dirección2 mombre')
+    f.TextoMixto("ID", "permanentAddress", permanentAddress, t)
+
+@then(u'Se da clic al botón de Submit')
 def step_impl(context):
-    print(u'STEP: Then Cargando su dirección')
+    print(u'STEP: Then Se da clic al botón de Submit')
+    f.ClickMixto("ID", "submit", t)
+    time.sleep(2)
+    context.driver.close()
